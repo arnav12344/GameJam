3eb15753b08185679a8d2d1d63af5bf48312cd1b
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2024-02-17 17:32:19 +0800

    Update YoyoController.cs
    Yoyo stuff

--- Assets/YoyoController.cs
+++ Assets/YoyoController.cs
@@ -7,20 +7,28 @@ public class YoyoController : MonoBehaviour
     // Start is called before the first frame update
     public SpringJoint2D joint;
     public float maxdist;
+    public float mindist;
 
     void Start()
     {
-        joint.distance = 1;
+        joint.distance = mindist;
     }
     
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        if (Input.GetAxis("Mouse Y") > 4)
         { 
             if (joint.distance < maxdist)
             {
                 joint.distance += 0.1f * maxdist * Time.deltaTime;
+            }
+        }
+        else if (Input.GetAxis("Mouse Y") < 0) 
+        {
+            if (joint.distance > mindist)
+            {
+                joint.distance -= 0.1f * maxdist * Time.deltaTime;
             }
         }
     }
