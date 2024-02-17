908e071cc4408f9611a624236eb0a799c415085f
NaelAnoob <116592348+NaelAnoob@users.noreply.github.com>
2024-02-17 17:15:52 +0800

    jj
    Yoyo changed

--- Assets/YoyoController.cs
+++ Assets/YoyoController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class YoyoController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public SpringJoint2D joint;
+    public float maxdist;
+
     void Start()
     {
-        
+        joint.distance = 1;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        { 
+            if (joint.distance < maxdist)
+            {
+                joint.distance += 0.1f * maxdist * Time.deltaTime;
+            }
+        }
     }
 }
